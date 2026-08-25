c6481b6c8434cc9a1bfe4218f558b16b02fa14cb
Neiland <rodrigo.naohan@gmail.com>
2026-08-25 19:17:58 -0300

    Maratona 2026 Esquenta - Bravo, bravo!

---
+++ provas/MaratonaBrasileira/2026/Esquenta/B.py
@@ -0,0 +1,14 @@
+
+letra = []
+
+for i in range(5):
+    letra.append(input())
+
+print(letra[0])
+print('Bravo, bravo!')
+print(letra[1])
+print('Bravo, bravo!')
+print(letra[2])
+print(letra[3])
+print(letra[4])
+print(letra[4])
