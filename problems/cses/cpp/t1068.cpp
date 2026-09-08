34f3fb6c517f067d4b3bc2813f8f3a7242f800be
Neiland <rodrigo.naohan@gmail.com>
2026-09-08 16:43:25 -0300

    Weird Algorithm

---
+++ problems/cses/cpp/t1068.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long n;
+    
+    cin >> n;
+
+    while (n != 1){
+        cout << n << ' ';
+
+        if (n % 2 == 1) n = 3*n + 1;
+        else n = n / 2;
+
+    }
+
+    cout << n << '\n';
+
+}
